75d8f36d9852aebdaec4e0b5cf6e6de4ed4ab28c
Woongcheol Yang <woongcheol@gmail.com>
2015-01-12 01:44:13 +0900

    Add update_hashtags callback to Status

--- app/models/status.rb
+++ app/models/status.rb
@@ -6,4 +6,16 @@ class Status < ActiveRecord::Base
   validates :user, presence: true
 
   scope :latest, -> { order(created_at: :desc).first }
+
+  after_save :update_hashtags
+
+  def update_hashtags
+    self.hashtags = extract_hashtags.map { |name| Hashtag.find_or_create_by(name: name) }
+  end
+
+  def extract_hashtags
+    names = description.scan(/(?<=#)[[:alpha:]][[[:alnum:]]-]*/).uniq
+    names.each { |name| yield name } if block_given?
+    names
+  end
 end
