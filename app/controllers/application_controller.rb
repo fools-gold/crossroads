ddd1b8efae946f2779fc4e2c2a9fc678e7f7a98b
Hwidong Bae <spilist@gmail.com>
2015-03-11 10:03:07 +0900

    Fix permitted parameters for sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).append [:team_id, :first_name, :last_name]
-    devise_parameter_sanitizer.for(:sign_up).append [:first_name, :last_name]
   end
 
   def user_timezone(&block)
