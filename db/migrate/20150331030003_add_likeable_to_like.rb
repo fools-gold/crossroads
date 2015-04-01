e60dcc5496cc7102b1eba4b5b870637d4f9193f6
Hwidong Bae <spilist@gmail.com>
2015-04-01 19:06:30 +0900

    Improve migration for adding likeable to like

--- db/migrate/20150331030003_add_likeable_to_like.rb
+++ db/migrate/20150331030003_add_likeable_to_like.rb
@@ -1,12 +1,7 @@
 class AddLikeableToLike < ActiveRecord::Migration
   def change
     rename_column :likes, :status_id, :likeable_id
-    add_column :likes, :likeable_type, :string
-
-    Like.find_each do |like|
-      like.update likeable_type: 'Status'
-    end
-
-    change_column_null :likes, :likeable_type, false
+    add_column :likes, :likeable_type, :string, null: false, default: 'Status'
+    change_column_default(:likes, :likeable_type, nil)
   end
 end
